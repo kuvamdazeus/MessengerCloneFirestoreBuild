3ea9d6a9979938dc2bec4ee029ee380d0b5544af
Kuvam Bhardwaj <52134060+kuvamdazeus@users.noreply.github.com>
2021-05-19 01:04:52 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,11 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB7pUDih13w9KG6bo_Vq-qoChH58xFkfk8",
+    apiKey: "get-your-own-api-key",
     authDomain: "messenger-clone-firestore.firebaseapp.com",
-    databaseURL: "https://messenger-clone-firestore-default-rtdb.firebaseio.com",
+    databaseURL: "******",
     projectId: "messenger-clone-firestore",
-    storageBucket: "messenger-clone-firestore.appspot.com",
+    storageBucket: "messenger-*******",
     messagingSenderId: "890907187167",
     appId: "1:890907187167:web:9077c342f8796ba92c966a",
     measurementId: "G-GD5ZBJQ390"
