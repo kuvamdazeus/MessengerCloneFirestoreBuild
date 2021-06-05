3ba7fcba72cf6ac6bb93cc5d2e561abe5623a1d0
Kuvam Bhardwaj <52134060+kuvamdazeus@users.noreply.github.com>
2021-06-05 13:46:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
 
       <div className="app">
         <Nav />
-        {/* Add a theme switch button */}
 
         <Switch>
           <Route exact path="/" component={Login} />
